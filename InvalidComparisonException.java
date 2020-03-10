a76ada9c0b3c2012cd672a485f1f7af62821f35d
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-03-10 13:31:01 -0700

    now includes square class the 2 exception classes

---
+++ InvalidComparisonException.java
@@ -0,0 +1,5 @@
+
+
+public class InvalidComparisonException extends RuntimeException{
+
+}
---
+++ Square.java
@@ -0,0 +1,21 @@
+
+
+public class Square {
+
+    public Square(){
+        this("NoNameSquare");
+    }
+
+    public Square(String name){
+        
+    }
+
+	public String getDistance(Square s1) {
+		return null;
+	}
+
+	public String getDistance(Point p1) {
+		return null;
+	}
+
+}
