e0ee87bd67123ffdcae23c895186875eaeab6dd6
Kenny Ta <kenny_ta@yahoo.com>
2020-03-14 16:22:02 -0700

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,6 +1,6 @@
 
 
-public class Square {
+public class Square extends Shape {
 
     public Square(){
         this("NoNameSquare");
@@ -16,6 +16,36 @@ public class Square {
 
 	public String getDistance(Point p1) {
 		return null;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String scale(int scalefactor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getDistance(Shape other) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
